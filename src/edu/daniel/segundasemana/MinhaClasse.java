80ce4261c882dfe4f4d9131a15f290fc338348e9
Daniel Gomes <danielgomesdeoliveira@outlook.com.br>
2023-01-21 11:43:12 -0300

    primeiro commit no intellij

---
+++ src/edu/daniel/segundasemana/MinhaClasse.java
@@ -0,0 +1,18 @@
+package edu.daniel.segundasemana;
+
+public class MinhaClasse {
+    public static void main (String [] args) {
+
+        String primeiroNome = "Daniel";
+        String segundoNome = "Gomes";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+
+    }
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+
+}
